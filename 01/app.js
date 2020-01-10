d224e54c9864ae9f4bf398994764973731752562
Michal <michal.ciombor@gmail.com>
2020-01-10 22:27:24 +0100

    solution no1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,6 @@
-console.log('DOM');
+let first = Array.from(document.querySelectorAll(".comments__item" && ".comments__item--newest"));
+let secundo =[];
+for (el of first){
+   secundo.push(el.hasAttribute("[data-info]"))
+}
+console.log(secundo.length);
