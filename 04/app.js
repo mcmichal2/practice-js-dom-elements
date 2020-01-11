201ed3faff98cec4c09638c756e36116efa77029
Michal <michal.ciombor@gmail.com>
2020-01-11 13:08:43 +0100

    answer1 and 2

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,41 @@
-console.log('DOM');
+
+// solution1:
+// const nav = document.createElement("nav");
+// const ul = document.createElement("ul");
+// const links = ["/", "/gallery", "/contact"];
+// const names=["start","galeria","kontakt"];
+
+// document.body.appendChild(nav);
+// document.querySelector("nav").appendChild(ul);
+// const ul1 = document.querySelector("ul");
+
+// for(let i =0; i<3; i++){
+//     const a = document.createElement("a");
+//     a.setAttribute("href",links[i]);
+//     a.innerText=names[i];
+//     const li = document.createElement("li");
+//     li.appendChild(a);   
+//    ul1.appendChild(li);
+// }
+
+// solution2
+const menuItems = [
+    {text: 'start', url: '/'},
+    {text: 'galeria', url: '/gallery'},
+    {text: 'kontakt', url: '/contact'},
+]
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+
+document.body.appendChild(nav);
+document.querySelector("nav").appendChild(ul);
+const ul1 = document.querySelector("ul");
+
+for(let i =0; i<menuItems.length; i++){
+    const a = document.createElement("a");
+    a.setAttribute("href",menuItems[i].url);
+    a.innerText=menuItems[i].text;
+    const li = document.createElement("li");
+    li.appendChild(a);   
+   ul1.appendChild(li);
+}
