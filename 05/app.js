3809b09ae9216545e58aad1e1e6da1e4038e0206
Michal <michal.ciombor@gmail.com>
2020-01-16 19:47:25 +0100

    finished

--- 05/app.js
+++ 05/app.js
@@ -1,21 +1,39 @@
-
-
 const curr = document.querySelector('.js-curr');
-
-
 const parent = curr.parentElement;
+const parentParent = parent.parentElement;
+const parentSibling = parent.nextElementSibling;
+const parentSecondSibling = parentSibling.nextElementSibling;
+const children= parent.children;
 const btn = document.createElement("button");
+const btn2 = document.createElement("button");
+const p = document.createElement("p");
+const p2 = document.createElement("p");
+const article = document.createElement("article");
+const h1 = document.createElement("h1");
+const buttonIndex = [...parentSecondSibling.children].findIndex(el=>el.classList.contains("article__btn"));
+//dodanie buttona 
 btn.innerText="usuń z koszyka";
 parent.appendChild(btn);
-const children= parent.children;
-
+//dodanie klas do rodzeństwa
 for(child of children){
     if(child.classList.contains("js-curr")){
-        console.log("da")
+       null
     }
     else{
         child.classList.add("sibling")
     }
 }
-const parentSibling = parent.nextElementSibling;
-parentSibling.setAttribute("title","nextElementSibling")
+//dodanie atrbutu title
+parentSibling.setAttribute("title","nextElementSibling");
+//dodanie paragrafu przed buttonem
+p.innerText="Lorem Ipsume extra paragraph";
+parentSecondSibling.insertBefore(p, parentSecondSibling.children[buttonIndex])
+//dodanie artykułu o takiej samej zawartości (jako pierwszy)
+article.setAttribute("class","articles__item article")
+h1.innerText="DODANY ARTYKUŁ";
+p2.innerText="tekst dodanego artykułu";
+btn2.innerText = 'Kupuje!';
+article.appendChild(h1);
+article.appendChild(p2);
+article.appendChild(btn2);
+parentParent.insertBefore(article,parent)
